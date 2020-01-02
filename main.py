5921d5de54bf59b813682d02bb55f45160b724e6
ehanson94 <ehanson94@gmail.com>
2020-01-02 14:23:26 -0500

    set up main.py with functions

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+
+def parse_json(filename):
+    # Your code here
+    pass
+
+
+def schedule_per_plant():
+    # Your code here
+    pass
+
+
+def final_schedule():
+    # Your code here
+    pass
+
+
+def weekend_filter():
+    # Your code here
+    pass
+
+
+def create_table():
+    # Your code here
+    pass
+
+
+def main():
+    # Your code here
+    pass
+
+
+if __name__ == "__main__":
+    main()
